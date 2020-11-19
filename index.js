3592812ead415d299f21eda5a213c15178168791
FOUIX <fouix@pc-155.home>
2020-11-19 18:43:49 +0100

    corection import

--- index.js
+++ index.js
@@ -5,23 +5,21 @@ const md5 = require("md5");
 const uid2 = require("uid2");
 const cors = require("cors");
 
-//app.use(cors());
-
 require("dotenv").config();
-
 const app = express();
+app.use(cors());
 
 const publicKey = process.env.PUBLIC_KEY;
 const privateKey = process.env.PRIVATE_KEY;
 
-app.get("/comics", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     // Générer le ts
     const ts = uid2(8);
     const hash = md5(ts + privateKey + publicKey);
 
     const response = await axios.get(
-      `http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+      `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
     );
     res.json(response.data.data);
   } catch (error) {
