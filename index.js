37d9b11f99cdbe2a4cc95db92fa80bfd497f4c51
FOUIX <fouix@macbook-pro-de-fouix.home>
2021-03-04 10:52:46 +0100

    route character id

--- index.js
+++ index.js
@@ -45,6 +45,23 @@ app.get("/comics", async (req, res) => {
   }
 });
 
+//route pour filtrer les comics via l'id des personnages
+
+app.get("/characterCommics", async (req, res) => {
+  try {
+    // Générer le ts
+    const ts = uid2(8);
+    const hash = md5(ts + privateKey + publicKey);
+
+    const response = await axios.get(
+      `http://gateway.marvel.com/v1/public/characters/{characterId}/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+    );
+    res.json(response.data.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server Started on port ${process.env.PORT}`);
 });
